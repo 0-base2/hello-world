e60acaea8e256bdcf7904ba9bc1574fdb513418c
Base-2 <avilatchaw@gmail.com>
2024-09-15 11:38:45 +0300

    FLUX Update

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,5 @@
 print('Our First Program:\n')
 print('Print "Hello, World!" to the standard output:\n')
 print('Here we go!...\n\nHello, World!\n')
-print('SUCCESS!!!\n')
+print('SUCCESS!!!\n'); print()
+print('Hello, from FLUXBOX!')
